2f6a05e5ce7217fb83793da0ddd45ad4cf2cbf7a
hu-r25 <g66586777@gmail.com>
2026-05-02 13:53:55 +0300

    Update anss.py

--- anss.py
+++ anss.py
@@ -1,5 +1,7 @@
-import tkinter as tk
-from tkinter import messagebox, scrolledtext
+import streamlit as st
+import re
+import matplotlib.pyplot as plt
+import numpy as np
 import re
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
